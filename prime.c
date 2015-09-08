e7fbd59dd67e5a4d3840e6e0e888f7d1ef0d4a58
Alexandr Kolesnik <alexandr.kolesnik.51@my.csun.edu>
2015-09-07 20:48:56 -0700

    Added a working prime function. Covered possible cases for the query string. Still need to make a parser for the query string. Program will work both through browser and as an executable.

--- prime.c
+++ prime.c
@@ -2,31 +2,42 @@
 #include <stdlib.h>
 
 void EmptyQuery();
+void Prime();
 
 int main (){
-
-	int number1, number2;
+	int num;
 	char* query = getenv("QUERY_STRING");
-	if (query != NULL){
+	if (query == NULL){
+		printf("Enter a number to see if it's prime: ");
+		scanf("%d", &num);
+		Prime(num);		
+	}else{
 		printf("Content-Type: text/html;charset=us-ascii\n\n");
 		printf("<!DOCTYPE html>\n<html>\n");
-	}
-	if (query[0] == '\0')
-		EmptyQuery();
-//	scanf("%d%d", &number1, &number2);
-//	printf("%d\n", number1*number2);
-
-	if (query != NULL)
+		if (query[0] == '\0')
+			EmptyQuery();
+		else 
+			printf("%s\n", query);
 		printf("\n</html>\n");
+	}
 }
 
 void EmptyQuery (){
-//	printf("<form action="http://www.csun.edu/~ak505188/cgi-bin/prime.cgi")>\n");
-	printf("Hello World!\n");
+	printf("<form action=\"http://www.csun.edu/~ak505188/cgi-bin/prime.cgi\")>\n");
+	printf("<div><label>Is it prime? <input name=\"num\" size=\"10\"></label></div>\n");
+	printf("<div><input type =\"submit\" value=\"Find out!\"></div>\n");
 }//VoidQuery
 
-
-
-
-
-
+void Prime(int num){
+	FILE *primes = fopen("primes1.txt", "r");
+	int div, prime = 1;
+	fscanf(primes, "%d", &div);
+	for (;div < num && prime == 1; fscanf(primes, "%d", &div)){
+	//	printf("%d\n", div);
+		if (num % div == 0)
+			prime = 0;
+	}
+	if (prime == 0)
+		printf("Not prime!\n");
+	else printf("Prime!\n");
+}//Prime
